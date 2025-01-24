3396d9e1c1524fa25f279986326da1f627761d1e
Krishnankit Dixit <krishnankitdixit@gmail.com>
2025-01-24 22:20:35 +0530

    Create filter functionality

---
+++ src/components/Fiter.jsx
@@ -0,0 +1,145 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
+function Filter({ setFilter }) {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownBtnRef = useRef();
+  const dropdownRef = useRef();
+  const [category, setCategory] = useState("Category...");
+  const titleRef = useRef();
+
+  // Add Eventlistener on body to close dropdown when clicked anywhere.
+  useEffect(() => {
+    function closeDropdown() {
+      setShowDropdown(false);
+      dropdownBtnRef.current.removeAttribute("open");
+    }
+
+    document.addEventListener("click", closeDropdown);
+
+    return () => document.removeEventListener("click", closeDropdown);
+  }, []);
+
+  function toggleDropdown(event) {
+    event.stopPropagation();
+    setShowDropdown(!showDropdown);
+
+    // Here open attribute is added to dropdown arrow conditionally
+    if (showDropdown) {
+      event.target.removeAttribute("open");
+    } else {
+      event.target.setAttribute("open", true);
+    }
+  }
+
+  return (
+    <div className="max-w-2xl mx-auto mb-4 px-8 py-4 sm:flex sm:items-center sm:gap-8 rounded shadow-sm">
+      <div className="w-full mb-4 sm:my-2 flex justify-between">
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Search"
+          className="px-2 py-1 outline-none border-b-2 border-indigo-600 w-full"
+          ref={titleRef}
+        />
+        <button className="outline-none">
+          <FaMagnifyingGlass
+            className="inline text-secondary ml-2 hover:text-gray"
+            onClick={() => {
+              setFilter(filter => ({
+                ...filter,
+                title: titleRef.current.value,
+              }));
+            }}
+          />
+        </button>
+      </div>
+      <div className="w-full relative flex">
+        <button
+          className={`relative inline-block w-full px-2 py-1 outline-none bg-white text-left text-slate-800 capitalize group ${showDropdown ? "open" : ""}`}
+          onClick={toggleDropdown}
+          ref={dropdownBtnRef}
+        >
+          {category}
+          <svg
+            className="absolute right-2 top-1/2 -translate-y-1/2 inline-block transition duration-150 group-open:-rotate-180"
+            width="12"
+            height="8"
+            viewBox="0 0 80 50"
+            preserveAspectRatio="none"
+          >
+            <path
+              className="transition duration-100 stroke-indigo-600"
+              d="M0 0 L40 50"
+              fill="none"
+              strokeLinecap="round"
+              strokeWidth="10"
+            />
+            <path
+              className="transition duration-100 stroke-indigo-600"
+              d="M40 50 L80 0"
+              fill="none"
+              strokeLinecap="round"
+              strokeWidth="10"
+            />
+          </svg>
+        </button>
+        <button className="outline-none">
+          <FaMagnifyingGlass
+            className="inline text-secondary ml-2 hover:text-gray"
+            onClick={() => {
+              setFilter(filter => ({
+                ...filter,
+                category
+              }));
+            }}
+          />
+        </button>
+        <Dropdown
+          show={showDropdown}
+          dropdownRef={dropdownRef}
+          setCategory={setCategory}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Dropdown({ show, dropdownRef, setCategory }) {
+  const [dropdownHeight, setDropdownHeight] = useState(0);
+
+  useEffect(() => {
+    if (show) {
+      setDropdownHeight(dropdownRef.current.scrollHeight);
+    }
+  }, [show]);
+
+  return (
+    // Tailwind does not support height so used inline style
+    <ul
+      className={`bg-gray-50 w-full absolute top-[110%] overflow-hidden right-0 text-primary shadow-sm transition-[max-height] duration-200`}
+      style={{
+        maxHeight: show ? dropdownHeight + "px" : "0",
+        padding: show ? "0.125rem 0" : "0",
+       }}
+      ref={dropdownRef}
+    >
+      {
+        ["clothes", "electronics", "furniture"].map(category => {
+          return (
+            <li
+              key={category}
+              className="px-2 py-1 text-slate-800 cursor-pointer transition duration-200 hover:bg-indigo-600 hover:text-gray-50 capitalize"
+              onClick={() => setCategory(category)}
+            >
+              {category}
+            </li>
+          );
+        })
+      }
+    </ul>
+  );
+}
+
+export default Filter;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { fireDB } from "../../firebaseConfig";
-import { FaMagnifyingGlass } from "react-icons/fa6";
 import ProductCard from "../components/ProductCard";
 import { useNavigate } from "react-router";
 import { checkAdmin } from "../helpers";
 import { globalContext } from "../context/globalState";
+import Filter from "../components/Fiter";
 
 function Home() {
   const { globalState: { currentUser } } = useContext(globalContext);
@@ -19,7 +19,13 @@ function Home() {
       console.log(error);
     })
   }
+  const [unfilteredProducts, setUnfilteredProducts] = useState();
   const [products, setProducts] = useState([]);
+
+  const [filter, setFilter] = useState({
+    title: null,
+    category: null,
+  });
 
   useEffect(() => {
     const collectionRef = collection(fireDB, "products");
@@ -34,6 +40,7 @@ function Home() {
       });
 
       setProducts(products);
+      setUnfilteredProducts(products);
     })
     .catch(error => {
       displayToast({
@@ -45,35 +52,27 @@ function Home() {
     });
   }, []);
 
+  // Filter the products based on filter value
+  useEffect(() => {
+    if (filter.title) {
+      const products = unfilteredProducts.filter(prod => {
+        console.log(prod.title, filter.title);
+        return prod.title.toLowerCase().includes(filter.title.toLowerCase())
+      });
+      setProducts(products);
+    }
+
+    if (filter.category) {
+      const products = unfilteredProducts.filter(prod => {
+        return prod.category.toLowerCase() == filter.category.toLowerCase()
+      });
+      setProducts(products);
+    }
+  }, [filter]);
+
   return (
     <div>
-      <div className="max-w-2xl mx-auto mb-4 px-8 py-4 sm:flex sm:items-center sm:gap-8 rounded shadow-sm">
-        <div className="w-full mb-4 sm:my-2 flex justify-between">
-          <input
-            type="text"
-            name="title"
-            id="title"
-            placeholder="Search"
-            className="px-2 py-1 outline-none border-b-2 border-gray w-full"
-          />
-          <button className="outline-none">
-            <FaMagnifyingGlass className="inline text-secondary ml-2 hover:text-gray" />
-          </button>
-        </div>
-        <div className="w-full">
-          <select
-            name="category"
-            id="category"
-            className="inline-block w-full px-2 py-1 border-2 rounded border-gray outline-none"
-            defaultValue=""
-          >
-            <option value="" disabled>Category</option>
-            <option value="clothes">Clothes</option>
-            <option value="electronics">Electronics</option>
-            <option value="furniture">Furniture</option>
-          </select>
-        </div>
-      </div>
+      <Filter setFilter={setFilter} />
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
         {
           products.map(product => {
