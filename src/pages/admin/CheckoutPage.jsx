3039fb2422d68ba17b81df42a9f85490c0f77fcf
Krishnankit Dixit <krishnankitdixit@gmail.com>
2025-01-28 12:31:13 +0530

    Update db prod quantity acc. to purchase

--- src/pages/admin/CheckoutPage.jsx
+++ src/pages/admin/CheckoutPage.jsx
@@ -8,6 +8,7 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { Link, useNavigate } from "react-router";
 import { destroyUserCart, formattedAddress } from "../../helpers";
 
+// ALERT Move this page into cart page as a component so that dependency on locastorage is removed
 function CheckoutPage() {
   const { globalState: { currentUser },
     displayToast
@@ -104,13 +105,15 @@ function CheckoutPage() {
       currency: "INR",
       description: "for testing purpose",
       handler: function (response) {
-        displayToast({
-          message: "Payment Successful",
-          type: "success",
-        });
-
         const paymentId = response.razorpay_payment_id;
 
+        // Reduce quantity of original items acc. to purchase
+        items.forEach(item => {
+          const prodRef = doc(fireDB, "products", item.id);
+          updateDoc(prodRef, { "quantity": item.quantity - item.cartQuantity });
+        });
+
+        // Create new order after purchase
         const orderInfo = {
           items: orderItems,
           amount: grandTotal,
@@ -138,7 +141,7 @@ function CheckoutPage() {
       },
 
       theme: {
-        color: "#2C74B3"
+        color: "rgb(99, 102, 241)",
       }
     };
 
@@ -149,7 +152,7 @@ function CheckoutPage() {
   return (
     <>
       <div className="sm:flex justify-between gap-10">
-        <div className="w-full mb-5 border-b-4 border-secondary">
+        <div className="w-full mb-5">
           <h1 className="text-xl font-primary font-bold">Order Summary</h1>
           <CartSummary cartItems={ items } />
         </div>
@@ -162,7 +165,7 @@ function CheckoutPage() {
                 <div
                   key={index}
                   onClick={() => setSelectedAddress(index)}
-                  className={`flex justify-between items-center p-2 my-2 cursor-pointer border border-secondary ${selectedAddress === index && "bg-selected"}`}
+                  className={`flex justify-between items-center p-2 my-2 cursor-pointer rounded shadow-sm ${selectedAddress === index && "bg-blue-100"}`}
                 >
                   <div>
                     <p>{ formattedAddress(address) }</p>
@@ -171,26 +174,26 @@ function CheckoutPage() {
                   {
                     selectedAddress === index &&
                     <svg
-                    viewBox="0 0 40 20"
-                    width="40"
-                    height="20"
-                    className="block ml-2"
-                  >
-                    <path
-                      d="M5 5 L10 15"
-                      strokeWidth="5"
-                      strokeLinejoin="round"
-                      strokeLinecap="round"
-                      className="stroke-secondary"
-                    />
-                    <path
-                      d="M10 15 L30 3"
-                      strokeWidth="5"
-                      strokeLinejoin="round"
-                      strokeLinecap="round"
-                      className="stroke-secondary"
-                    />
-                  </svg>
+                      viewBox="0 0 40 20"
+                      width="40"
+                      height="20"
+                      className="block ml-2"
+                    >
+                      <path
+                        d="M5 5 L10 15"
+                        strokeWidth="5"
+                        strokeLinejoin="round"
+                        strokeLinecap="round"
+                        className="stroke-green-300"
+                      />
+                      <path
+                        d="M10 15 L30 3"
+                        strokeWidth="5"
+                        strokeLinejoin="round"
+                        strokeLinecap="round"
+                        className="stroke-green-300"
+                      />
+                    </svg>
                   }
                 </div>
               );
@@ -198,16 +201,16 @@ function CheckoutPage() {
           }
           <form
             action={handleSubmit}
-            className="border-2 border-secondary mt-4 p-2 text-center"
+            className="mt-4 px-4 py-2 text-center text-sm shadow-sm"
           >
-            <h3 className="mt-2">Dilver to another address?</h3>
+            <h3 className="my-2 text-base">Dilver to another address?</h3>
             <div className="lg:flex justify-between gap-5">
               <input
                 type="text"
                 name="houseName"
                 id="houseName"
                 placeholder="House name..."
-                className="inline-block w-full px-2 py-1 mb-2 outline-none border-b-2 border-secondary"
+                className="inline-block w-full px-2 py-1 mb-2 outline-none border-b-2 border-indigo-500"
                 value={address.houseName}
                 onChange={handleChange}
               />
@@ -216,7 +219,7 @@ function CheckoutPage() {
                 name="streetName"
                 id="streetName"
                 placeholder="Street name..."
-                className="inline-block w-full px-2 py-1 mb-2 outline-none border-b-2 border-secondary"
+                className="inline-block w-full px-2 py-1 mb-2 outline-none border-b-2 border-indigo-500"
                 value={address.streetName}
                 onChange={handleChange}
               />
@@ -227,7 +230,7 @@ function CheckoutPage() {
                 name="city"
                 id="city"
                 placeholder="City..."
-                className="inline-block w-full px-2 py-1 mb-2 outline-none border-b-2 border-secondary"
+                className="inline-block w-full px-2 py-1 mb-2 outline-none border-b-2 border-indigo-500"
                 value={address.city}
                 onChange={handleChange}
               />
@@ -236,7 +239,7 @@ function CheckoutPage() {
                 name="pincode"
                 id="pincode"
                 placeholder="pincode..."
-                className="inline-block w-full px-2 py-1 mb-2 outline-none border-b-2 border-secondary"
+                className="inline-block w-full px-2 py-1 mb-2 outline-none border-b-2 border-indigo-500"
                 value={address.pincode}
                 onChange={handleChange}
               />
@@ -245,13 +248,13 @@ function CheckoutPage() {
                 name="state"
                 id="state"
                 placeholder="State..."
-                className="inline-block w-full px-2 py-1 mb-2 outline-none border-b-2 border-secondary"
+                className="inline-block w-full px-2 py-1 mb-2 outline-none border-b-2 border-indigo-500"
                 value={address.state}
                 onChange={handleChange}
               />
             </div>
             <button
-              className={`px-4 py-2 mt-2 mx-auto text-sm text-secondary bg-white border rounded border-secondary ${ valid ? "" : "text-red border-red" } transition duration-100`}
+              className={`px-4 py-2 mt-2 mx-auto text-sm text-indigo-500 bg-white border rounded border-indigo-500 ${ valid ? "" : "text-red-600 border-red-600" } hover:bg-indigo-500 hover:text-white transition duration-100`}
               disabled={!valid}
             >
               Add address
@@ -261,13 +264,13 @@ function CheckoutPage() {
       </div>
       <div className="flex justify-between">
       <button
-        className="text-secondary bg-white py-2 px-4 border border-secondary"
+        className="text-slate-800 bg-white py-2 px-4 shadow-sm"
       >
         <FaArrowLeft className="inline mr-2" />
         <Link to="/cart">Back to KART</Link>
       </button>
       <button
-        className="text-white bg-secondary py-2 px-4"
+        className="text-white bg-indigo-500 py-2 px-4 shadow-lg"
         onClick={handlePayment}
       >
         Proceed to payment
